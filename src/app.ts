622c7005772b47804689e56321f128db668f0fd4
rafaelbchs <rafaelbchacins@gmail.com>
2022-09-20 10:15:55 -0500

    added validation

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,36 @@ function Autobind(_: any, __: string, descriptor: PropertyDescriptor){
     return adjDescriptor;
 }
 
+// validation
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable){
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0 
+    }
+    if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === "number") {
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -41,7 +71,24 @@ class ProjectInput {
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
 
-        if (!enteredTitle.trim().length || !enteredDescription.trim().length || !enteredPeople.trim().length) {
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true
+        }
+
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true
+        }
+
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 9
+        }
+
+        if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
             alert("Invalid input, please try again")
             return; 
         } else {
