774237f5caa24435dff8ecd5717da2a2aecfa14f
rafaelbchs <rafaelbchacins@gmail.com>
2022-09-26 09:51:25 -0500

    refactored code

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,15 @@
+namespace App {
+    // autobind decorator
+
+export function Autobind(_: any, __: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get () {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+}
+}
